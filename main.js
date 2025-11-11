c672736d195196aadbb7da5514e4a55d69d76eb4
rianti ramadani <riantiramadani221@gmail.com>
2025-11-11 10:27:38 +0700

    latihan 15

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+function cekNilai() {
+// mengambil value dari elemen input nilai 
+// simpan ke dalam variabel bernama"nilai"
+let nilai = Number(document.getElementById('nilai').value)
+
+  // menginisiasi variabel "hasil" isiny string kosong""
+  let hasil = ""
+  
+  //menentukan angka A B C D berdasarkan isi variabel "nilai"
+  //menggunakan percabangan if (jika)
+  //dan membandingkan variabel "nilai"
+  if (nilai >=88){
+    hasil ="Nilai A (sangat baik)"
+  } else if (nilai >= 75) {
+    hasil = "Nilai B (baik)"
+  } else if (nilai >= 50) {
+    hasil = "Nilai C (cukup)"
+  } else if (nilai >= 0) {
+    hasil = "Nilai D (kurang)"
+  }
+    // tampilkan isi variabel "hasil"ke elemen "hasil"di html 
+    document.getElementById('hasil').innerText = hasil
+  }
